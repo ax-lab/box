56368d0fba7f02e2966e19f3231aea3b10b74b30
ax-ab <98651633+ax-lab@users.noreply.github.com>
2023-11-28 21:49:52 +1300

    bit: add demo program idea

--- rust/bit/bit.rs
+++ rust/bit/bit.rs
@@ -1,5 +1,7 @@
 use bit::*;
 
+const SKIP_CODE: bool = true;
+
 fn main() {
 	if let Err(err) = run() {
 		eprintln!("\nError: {err}\n");
@@ -17,7 +19,13 @@ fn run() -> Result<()> {
 
 	for arg in std::env::args().skip(1) {
 		let src = store.load_source(arg)?;
-		show_tokens(&mut lexer, src)?;
+		if !run_numbers(src) {
+			std::process::exit(1);
+		}
+	}
+
+	if SKIP_CODE {
+		return Ok(());
 	}
 
 	let code = r#"
@@ -55,4 +63,56 @@ fn show_tokens<T: Grammar>(lexer: &mut Lexer<T>, src: Source) -> Result<()> {
 	println!();
 
 	Ok(())
+}
+
+//====================================================================================================================//
+// DEMO - Running numbers
+//====================================================================================================================//
+
+/*
+	Tokens
+
+	EOL -> break by line
+	INT -> sum
+	INT -> print themselves reporting range
+
+*/
+
+pub fn run_numbers(src: Source) -> bool {
+	let mut lexer = NumLexer::new();
+	let mut input = src.span();
+	let mut pos = Pos::start();
+
+	let tokens = lexer.tokenize(&mut input, &mut pos);
+	if input.len() > 0 {
+		let line = input.text().lines().next().unwrap();
+		let name = src.name();
+		eprintln!("\n[Error]\n| failed to parse input `{line}`\n| at {name}:{pos}\n");
+		return false;
+	}
+
+	println!("\n■■■ RUN NUMBERS: {} ({} bytes) ■■■", src.name(), src.len());
+	println!();
+	for token in tokens {
+		println!("- {token:?}");
+	}
+	println!();
+
+	true
+}
+
+pub struct NumLexer {
+	lexer: Lexer<BasicGrammar>,
+}
+
+impl NumLexer {
+	pub fn new() -> Self {
+		let mut lexer = Lexer::new(BasicGrammar);
+		lexer.add_symbols([",", ";"]);
+		Self { lexer }
+	}
+
+	pub fn tokenize<'a>(&mut self, span: &mut Span<'a>, pos: &mut Pos) -> Vec<Token<'a>> {
+		self.lexer.tokenize(span, pos)
+	}
 }
